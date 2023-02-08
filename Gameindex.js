69fd322ec440d4ee242094a74377935d0d9b6427
Iiquids <83255875+Iiquids@users.noreply.github.com>
2023-02-08 15:26:51 +0000

    Fixed Width and Height

--- Gameindex.js
+++ Gameindex.js
@@ -42,7 +42,7 @@ canvas.width = 1000;
 
 var settings = {
     "PlayerSpeed": 10,
-    "PlayerSize": [window.innerHeight * 2, window.innerHeight * 2],
+    "PlayerSize": [30, (30)*2],
     "Players": 1,
     "PlayerColors": ["red", "green", "blue"]
 }
@@ -54,8 +54,8 @@ var frameSettings = {
 
 //Make background and sutff
 
-var pla1 = canvas.getContext("2d")
-var pla2 = canvas.getContext("2d")
+var pla1 = canvas.getContext("2d");
+var pla2 = canvas.getContext("2d");
 
 var playersInCanvas = {
     "Player1": {"Color": settings.PlayerColors[0], "Width": settings.PlayerSize[0], "Height": settings.PlayerSize[1], "PosX": canvas.width / 2, "PosY": canvas.height / 2},
@@ -63,8 +63,6 @@ var playersInCanvas = {
 }
 pla1.fillStyle = playersInCanvas.Player1.Color;
 pla2.fillStyle = playersInCanvas.Player2.Color;
-
-
 
 
 var state = {
@@ -95,17 +93,17 @@ function movePlayer() {
         }
     }
     if (keysDown.S == true) {
-        if ((playersInCanvas.Player1.PosY + settings.PlayerSpeed) <= canvas.height - 17) {
+        if ((playersInCanvas.Player1.PosY + settings.PlayerSpeed) <= canvas.height - settings.PlayerSize[1]) {
             playersInCanvas.Player1.PosY = playersInCanvas.Player1.PosY + settings.PlayerSpeed;
-        } else if (playersInCanvas.Player1.PosY + settings.PlayerSpeed >= canvas.height - 17) {
-            playersInCanvas.Player1.PosY = canvas.height - 17;
+        } else if (playersInCanvas.Player1.PosY + settings.PlayerSpeed >= canvas.height - settings.PlayerSize[1]) {
+            playersInCanvas.Player1.PosY = canvas.height - settings.PlayerSize[1];
         }
     }
     if (keysDown.D == true) {
-        if (playersInCanvas.Player1.PosX + settings.PlayerSpeed <= canvas.width-17) {
+        if (playersInCanvas.Player1.PosX + settings.PlayerSpeed <= canvas.width - settings.PlayerSize[0]) {
             playersInCanvas.Player1.PosX = playersInCanvas.Player1.PosX + settings.PlayerSpeed;
-        } else if (playersInCanvas.Player1.PosX + settings.PlayerSpeed >= canvas.width-17) {
-            playersInCanvas.Player1.PosX = canvas.width-17;
+        } else if (playersInCanvas.Player1.PosX + settings.PlayerSpeed >= canvas.width - settings.PlayerSize[0]) {
+            playersInCanvas.Player1.PosX = canvas.width - settings.PlayerSize[0];
         }
     }
     if (keysDown.Up == true) {
@@ -123,35 +121,30 @@ function movePlayer() {
         }
     }
     if (keysDown.Down == true) {
-        if (playersInCanvas.Player2.PosY + settings.PlayerSpeed <= canvas.height-17) {
+        if (playersInCanvas.Player2.PosY + settings.PlayerSpeed <= canvas.height - settings.PlayerSize[1]) {
             playersInCanvas.Player2.PosY = playersInCanvas.Player2.PosY + settings.PlayerSpeed;
-        } else if (playersInCanvas.Player2.PosY + settings.PlayerSpeed >= canvas.height - 17) {
-            playersInCanvas.Player2.PosY = canvas.height - 17;
+        } else if (playersInCanvas.Player2.PosY + settings.PlayerSpeed >= canvas.height - settings.PlayerSize[1]) {
+            playersInCanvas.Player2.PosY = canvas.height - settings.PlayerSize[1];
         }
     }
     if (keysDown.Right == true) {
-        if (playersInCanvas.Player2.PosX + settings.PlayerSpeed <= canvas.width-17) {
+        if (playersInCanvas.Player2.PosX + settings.PlayerSpeed <= canvas.width - settings.PlayerSize[0]) {
             playersInCanvas.Player2.PosX = playersInCanvas.Player2.PosX + settings.PlayerSpeed;
-        } else if (playersInCanvas.Player2.PosX + settings.PlayerSpeed >= canvas.width-17) {
-            playersInCanvas.Player2.PosX = canvas.width-17;
+        } else if (playersInCanvas.Player2.PosX + settings.PlayerSpeed >= canvas.width - settings.PlayerSize[0]) {
+            playersInCanvas.Player2.PosX = canvas.width - settings.PlayerSize[0];
         }
     }
 }
-
-var exmapleNode = canvas.getContext("2d");
-exmapleNode.fillStyle = playersInCanvas.Player1.Color;
 
 function loadPlayers() { // Loads and draws Players.
     // clearRect First else dumbass thing won't work
     // Bryan probably coded this dumb clearRect Thing cause its mad annyoing.... -_- UwU
 
-    pla2.clearRect(0, 0, playersInCanvas.Player2.Width, playersInCanvas.Player2.Height);
-    pla1.clearRect(0, 0, playersInCanvas.Player1.Width, playersInCanvas.Player1.Height);
-    exmapleNode.clearRect(0, 0, playersInCanvas.Player1.Width, playersInCanvas.Player1.Height); // EXMAPLE
+    pla2.clearRect(0, 0, canvas.width, canvas.height);
+    pla1.clearRect(0, 0, canvas.width, canvas.height);
 
-    pla1.fillRect(playersInCanvas.Player1.PosX, playersInCanvas.Player1.PosY, 20, 20);
-    pla2.fillRect(playersInCanvas.Player2.PosX, playersInCanvas.Player2.PosY, 20, 20);
-    exmapleNode.fillRect(0, pla1.Width, 20, 20); // EXMAPLE
+    pla1.fillRect(playersInCanvas.Player1.PosX, playersInCanvas.Player1.PosY, playersInCanvas.Player1.Width, playersInCanvas.Player1.Height);
+    pla2.fillRect(playersInCanvas.Player2.PosX, playersInCanvas.Player2.PosY, playersInCanvas.Player2.Width, playersInCanvas.Player2.Height);
 }
 
 function draw() {
@@ -161,7 +154,6 @@ function draw() {
 function checkTagged() {
     getSize1 = {"w": playersInCanvas.Player1.Width, "h": playersInCanvas.Player1.Height};
     getSize2 = {"w": playersInCanvas.Player2.Width, "h": playersInCanvas.Player2.Height};
-
 
 }
 
